77b421e35993e3ec6d7a8fc2a37c2bbf935e2092
Rohid JETHA <rohidjetha@me.com>
2017-02-16 13:41:00 +0100

    generate vhost file, parameters to do @wodzy

--- generate_vh.sh
+++ generate_vh.sh
@@ -1,3 +1,29 @@
 #!/bin/bash
 
+echo "<VirtualHost *:80>
+	            AddDefaultCharset utf-8
+	            ServerAdmin 
+	            ServerName 
+	            ServerAlias 
+	            DocumentRoot 
+	            
+	            <Directory >
+	             AllowOverride All
+	             Order allow,deny
+	             allow from all
+
+	            <IfModule mod_rewrite.c>
+	              Options -MultiViews
+	              Options -Indexes
+	              RewriteEngine On
+	              RewriteCond %{REQUEST_FILENAME} !-f
+	              RewriteRule ^ index.php [L]
+	            </IfModule>
+
+	            </Directory>
+
+	            ErrorLog /var/log/apache2/-error_log
+	            TransferLog /var/log/apache2/-access_log
+</VirtualHost>" > ~/Desktop/$1.conf
+
 ls;
