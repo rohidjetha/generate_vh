d721a966dfef38cc63f37e2b07bd9c65d5510cdb
Walid Oukaci <walid@oukaci.com>
2017-02-17 23:30:09 +0100

    edit vhost

--- generate_vh.sh
+++ generate_vh.sh
@@ -15,16 +15,8 @@ echo "<VirtualHost *:80>
 	             AllowOverride All
 	             Order allow,deny
 	             allow from all
-
-	            <IfModule mod_rewrite.c>
-	              Options -MultiViews
-	              Options -Indexes
-	              RewriteEngine On
-	              RewriteCond %{REQUEST_FILENAME} !-f
-	              RewriteRule ^ index.php [L]
-	            </IfModule>
-
-	            </Directory>
+	             #other guidelines here if needed..
+                    </Directory>
 
 	            ErrorLog /var/log/apache2/$1-error_log
 	            TransferLog /var/log/apache2/$1-access_log
